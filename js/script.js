e9db33db555d569a382986e236b53c5d49ca1bca
Arterm Serb <artemiys-serb@mail.ru>
2022-08-11 20:35:44 +0300

    Hero

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 const burger = document.querySelector('.menu-icon')
 const menu = document.querySelector('.menu')
 const body = document.body
+
+burger.addEventListener("click", () =>{
+   burger.classList.toggle("_active")
+   menu.classList.toggle("_active")
+   body.classList.toggle("_lock")
+})
