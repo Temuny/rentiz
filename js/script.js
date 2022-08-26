4ffd50ae57a25742cc00440db127d8fefdf6db7d
Arterm Serb <artemiys-serb@mail.ru>
2022-08-26 13:45:27 +0300

    Исправление

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@ burger.addEventListener("click", () =>{
    menu.classList.toggle("_active")
    body.classList.toggle("_lock")
 })
+/*------------------------------Filterdropdown-----------------------------------------*/
+
+const filter = document.querySelector(".filter")
+const items = filter.querySelectorAll(".filter__block")
+
+items.forEach(item => {
+   item.addEventListener("click", event => {
+      item.querySelector(".block-filter__dropdown").classList.toggle("_active")
+      item.querySelector(".block-filter__icon").classList.toggle("_active")
+      
+      if(event.target.classList.contains("block-filter__item")) {
+         item.querySelector(".block-filter__value").textContent = event.target.textContent;
+      }
+      console.log(event.target)
+   })
+})
+
+
