225fa834842d72b317d4821b7011e97096017682
Arterm Serb <artemiys-serb@mail.ru>
2022-09-16 17:58:17 +0300

    Swiper

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const burger = document.querySelector('.menu-icon')
 const menu = document.querySelector('.menu')
 const body = document.body
 
-burger.addEventListener("click", () =>{
+burger.addEventListener("click", () => {
    burger.classList.toggle("_active")
    menu.classList.toggle("_active")
    body.classList.toggle("_lock")
@@ -16,12 +16,20 @@ items.forEach(item => {
    item.addEventListener("click", event => {
       item.querySelector(".block-filter__dropdown").classList.toggle("_active")
       item.querySelector(".block-filter__icon").classList.toggle("_active")
-      
-      if(event.target.classList.contains("block-filter__item")) {
+
+      if (event.target.classList.contains("block-filter__item")) {
          item.querySelector(".block-filter__value").textContent = event.target.textContent;
       }
       console.log(event.target)
    })
 })
 
+/*----------------------------------Swiper----------------------------------------*/
 
+const swiper = new Swiper('.swiper', {
+   // Navigation arrows
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+});
